a0fe646c5d0f4ddd66e74d6d0f137e1516742ab5
Ajr Wum <ajr.wum@gmail.com>
2022-01-07 17:39:13 +0100

    iteration 3 ok

--- routes/drones.js
+++ routes/drones.js
@@ -23,11 +23,29 @@ router.get('/drones', (req, res, next) => {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+
+  // loading the create form
+  res.render('drones/create-form');
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+
+  // loading the user's input
+  const userInput = req.body;
+  console.log(userInput);
+
+  // inserting this new drone into db
+  droneModel.create(userInput)
+  .then(newDrone => {
+    res.redirect('/drones');
+  })
+  .catch(e => {
+    // making sure the error is properly handled
+    next(e);
+  })
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
