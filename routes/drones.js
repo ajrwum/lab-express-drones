4c9d4534bfca14f3b3337198bd9557de9bbfc405
Ajr Wum <ajr.wum@gmail.com>
2022-01-07 18:46:22 +0100

    iteration 4 & 5 ok

--- routes/drones.js
+++ routes/drones.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// loading mongoose to check documents' id
+const mongoose = require('mongoose');
+
 // require the Drone model here
 const droneModel = require('./../models/Drone.model');
 
@@ -11,7 +14,6 @@ router.get('/drones', (req, res, next) => {
   // finding all drones in db to display them
   droneModel.find()
   .then(allDrones => {
-    console.log(allDrones);
     res.render('drones/list.hbs', { allDrones });
   })
   .catch(e => {
@@ -34,7 +36,6 @@ router.post('/drones/create', (req, res, next) => {
 
   // loading the user's input
   const userInput = req.body;
-  console.log(userInput);
 
   // inserting this new drone into db
   droneModel.create(userInput)
@@ -51,16 +52,75 @@ router.post('/drones/create', (req, res, next) => {
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+
+  // loading the drone to display it for update
+  droneModel.findById(req.params.id)
+  .then(drone => {
+    // rendering the uopdate form, passing the drone to update
+    res.render(`drones/update-form`, { drone });
+  })
+  .catch(e => {
+    // making sure the error is properly handled
+    next(e);
+  })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
 
-router.post('/drones/:id/delete', (req, res, next) => {
+  // checking the id
+  if (mongoose.isValidObjectId(req.params.id)) {
+    // updating the drone in the db
+    droneModel.findByIdAndUpdate(req.params.id, req.body)
+    .then(updated => {
+      // redirecting to the list of
+      res.redirect('/drones');
+    })
+    .catch(e => {
+      // making sure the error is properly handled
+      next(e);
+    })
+  }
+  else {
+    try {
+      // throwing an error to pass to next()
+      throw new Error('Invalid id');
+    }
+    catch (e) {
+      // making sure the error is properly handled
+      next(e);
+    }
+  }
+});
+  
+router.get('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+
+  // checking the id
+  if (mongoose.isValidObjectId(req.params.id)) {
+    // updating the drone in the db
+    droneModel.findByIdAndDelete(req.params.id)
+    .then(updated => {
+      // redirecting to the list of
+      res.redirect('/drones');
+    })
+    .catch(e => {
+      // making sure the error is properly handled
+      next(e);
+    })
+  }
+  else {
+    try {
+      // throwing an error to pass to next()
+      throw new Error('Invalid id');
+    }
+    catch (e) {
+      // making sure the error is properly handled
+      next(e);
+    }
+  }  
 });
 
 module.exports = router;
