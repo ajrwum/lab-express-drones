63eb008b1cbd271705e5d6b88c6ea0c8c790ebfe
Ajr Wum <ajr.wum@gmail.com>
2022-01-07 16:30:40 +0100

    iteration 0 & 1 ok

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,16 @@
 // Iteration #1
+
+// load necessary elements from mongoose
+const { model, Schema } = require('mongoose');
+
+// creating the Schema for a drone document
+const droneSchema = new Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number
+});
+
+// creating the model with the validating schema
+const droneModel = model("drone", droneSchema);
+
+module.exports = droneModel;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,40 @@
 // Iteration #1
+
+// some init
+const drones = [
+  {
+    name: "Creeper XL 500",
+    propellers: 3,
+    maxSpeed: 12
+  },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+// conecting to the db by executing the db config file
+require('./../db/index');
+
+// loading the model to validate the data to seed
+const droneModel = require('../models/Drone.model');
+
+// setting the function as an IIFE
+(async function() {
+  try{
+    // being at dev stage, cleaning first th db
+    const { deletedCount } = await droneModel.deleteMany();
+    console.log(`DB clean successful: ${deletedCount} deleted.`);
+
+    // now inserting the data
+    const created = await droneModel.create(drones);
+    console.log(`DB seeding successful: ${created.length} created`);
+
+    // exiting the seeding process and closing the db connection
+    process.exit();
+  }
+  catch (e) {
+    console.error(e);
+    // exiting the seeding process and incidentally closing the db connection 
+    process.exit();
+  }
+})();
+
